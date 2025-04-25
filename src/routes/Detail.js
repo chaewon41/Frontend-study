3fa508e82c4f24fc6e6a8aa66bddfc0850300d85
chaewon41 <chaew0m3x@gmail.com>
2025-04-25 20:33:22 +0900

    Parameters

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,20 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 function Detail() {
-    return <h1>Detail</h1>
+    const {id} = useParams();
+    const getMovie = async () => {
+        const json = await ( await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
+        setDeatil(json.data.movie)
+    }
+    const [detail, setDeatil] = useState([]);
+    useEffect(() => {
+       getMovie()
+    }, [])
+    console.log(detail)
+    return (<div>
+        <h1>Detail</h1>
+        <a href={detail.url}>{detail.url}</a>
+        </div>)
 }
 
 export default Detail;
