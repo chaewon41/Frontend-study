28b0ea0bf3fbd0cf09e9ae75f09f7e2a7a4ef392
chaewon41 <chaew0n3x@gmail.com>
2025-04-25 15:48:51 +0900

    처음 커밋!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import styles from "./App.module.css"
+import { useState, useEffect } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  // coin list가 처음으로 render 되면 함수를 즉시 실행시키고 싶음 
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json) => 
+      setCoins(json),
+      setLoading(false)
+  );
+  }, [])
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? <strong>Loading...</strong> :
+      <select>
+        {coins.map((coin) => 
+        <option key={coin.id}>
+          {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
+        </option>)}
+      </select>
+      }
     </div>
   );
 }
